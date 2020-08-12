e08d32c71db04ab9d35d577c2a6c700baabfc24d
Matthew Weiss <heyredhat@chip.local>
2020-08-12 05:44:09 -0400

    !

--- examples/osc_coherent_basis.py
+++ examples/osc_coherent_basis.py
@@ -1,13 +1,16 @@
 import qutip as qt
 import numpy as np
 import vpython as vp
+from magic import *
+
 vp.scene.width = 1000
 vp.scene.height = 800
 
 CALC_ROOTS = True
+CALC_SPHERE = True
 
 dt = 0.01
-n = 20
+n = 8
 a = qt.destroy(n)
 Q = qt.position(n)
 QL, QV = Q.eigenstates()
@@ -24,7 +27,7 @@ def oscillator_roots(q):
 	n = q.shape[0]
 	poly = [(c/np.sqrt(np.math.factorial(n-i))) for i, c in enumerate(q.full().T[0][::-1])]
 	#print(poly)
-	return np.roots(poly)
+	return [c.conjugate() for c in np.roots(poly)]
 
 state = qt.basis(n,0)
 amps = [state.overlap(v) for v in QV]
@@ -44,10 +47,20 @@ vcs = [[vp.arrow(pos=vp.vector(x, y+12, 0), color=vp.color.green,\
 				 axis=vp.vector(cs[i][j].real, cs[i][j].imag,0))
 			for j, y in enumerate(grid)] for i, x in enumerate(grid)]
 
+if CALC_ROOTS or CALC_SPHERE:
+	C = oscillator_roots(state)
+
 if CALC_ROOTS:
-	vC = [vp.sphere(color=vp.color.magenta, radius=0.15,\
+	vC = [vp.sphere(color=vp.color.magenta, radius=0.15,opacity=0.8,\
 				   pos=vp.vector(r.real, r.imag+12, 0))\
-						for r in oscillator_roots(state)]
+						for r in C]
+
+if CALC_SPHERE:
+	vsphere = vp.sphere(color=vp.color.blue, opacity=0.3, pos=vp.vector(0,4,0))
+	xyzs = [c_xyz(r) for r in C]
+	vsC = [vp.sphere(radius=0.15,opacity=0.8,\
+				    pos=vsphere.pos+vp.vector(*xyz))\
+						for xyz in xyzs]
 
 def keyboard(e):
 	global state, n, NV, H, U
@@ -85,18 +98,34 @@ while True:
 		for j, y in enumerate(grid):
 			vcs[i][j].axis = vp.vector(cs[i][j].real, cs[i][j].imag,0)
 
-	if CALC_ROOTS:
+	if CALC_ROOTS or CALC_SPHERE:
 		if T % 5 == 0:
 			C = oscillator_roots(state)
+
+	if CALC_ROOTS:		
+		if T % 5 == 0:	
 			if len(vC) > len(C):
 				for i in range(len(vC) - len(C)):
 					vC[0].visible = False
 					del vC[0]
 			elif len(C) > len(vC):
 				for i in range(len(C) - len(vC)):
-					vC.append(vp.sphere(color=vp.color.magenta, radius=0.15, visible=True))
+					vC.append(vp.sphere(color=vp.color.magenta, radius=0.15, opacity=0.8))
 
 			for i,r in enumerate(C):
 				vC[i].pos = vp.vector(r.real, r.imag+12, 0)
 
+	if CALC_SPHERE:
+		if T % 5 == 0:
+			if len(vsC) > len(C):
+				for i in range(len(vsC) - len(C)):
+					vsC[0].visible = False
+					del vsC[0]
+			elif len(C) > len(vsC):
+				for i in range(len(C) - len(vsC)):
+					vsC.append(vp.sphere(radius=0.15, opacity=0.8))
+			xyzs = [c_xyz(r) for r in C]
+			for i, xyz in enumerate(xyzs):
+				vsC[i].pos = vsphere.pos+vp.vector(*xyz)
+							
 	T += 1
